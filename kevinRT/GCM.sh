61e36d2c05cf3bead1db59c2b7b7dea8d2a1b72f
Kevin <shaoqihen@gmail.com>
2025-12-17 22:51:19 -0600

    kevinRT

---
+++ kevinRT/GCM.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=GCM_uranus
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=12
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=2-12:30:00
+#SBATCH --account=chengcli1 
+#SBATCH --partition=standard
+#SBATCH --output=GCM_12_16.log
+module load openmpi
+mpiexec -n 96 uranusnn.release -i uranusnn.inp
